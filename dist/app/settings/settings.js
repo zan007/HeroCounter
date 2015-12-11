97e93eaefe7ac6f7139ad35b1dead438959d6755
pploszaj91 <pploszaj91@gmail.com>
2015-12-11 21:40:00 +0100

    release

--- dist/app/settings/settings.js
+++
@@ -1,9 +0,0 @@
-angular.module('settings', ['dataSource']).
-
-controller('settingsCtrl', ['$scope', '$rootScope', 'dataSource',
-    function($scope, $rootScope, dataSource) {
-    	$scope.model = $rootScope.model;
-    	
-		
-    }
-]);
